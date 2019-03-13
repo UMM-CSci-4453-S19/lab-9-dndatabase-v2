08051f9122bd62ada9071f62637754d10fa7f1cc
bella148 <bella148@morris.umn.edu>
2019-03-13 16:35:28 -0500

    Finish communicating with client/server

--- express.js
+++ express.js
@@ -38,15 +38,69 @@ var processCurrentTransaction = function(rows)
 	});
 }
 
-// On button click
-// query information about item
-// put information about item into transaction table
-// refresh transaction table
-// :)
+// On Button Click...
+// Get information on the price of an item based on ID
+// Get information about the item ID in the context of transactions (If there already an ID? If so, how many items are in it)
+// Attempt to insert into the tranaction table (If ID already exists, do something differently)
+// Send the updated transaction table back to Client
 
-var getInventoryItemInfo = function(sql)
+var DoQuery = function(sql)
 {
 	return dbf.query(mysql.format(sql));
+}
+
+var getPriceFromID = function(id)
+{
+	var sql = "SELECT * FROM prices WHERE id = " + id
+	return DoQuery(sql);
+
+	//Know: ID and Price
+	//Missing: Quantity
+}
+
+var doesItemExistInTransaction = function(rows)
+{
+	var id = rows[0].id;
+	
+	var sql = "SELECT * FROM transaction WHERE id = " + id;
+	var result = Promise.resolve(DoQuery(sql));
+
+	result.then(function(val)
+	{
+		if(val[0] == null)
+		{
+			//Item does not exist in transactions
+			return addToTransaction(rows);
+			
+		}
+		else
+		{
+			return updateTransactionTable(rows);
+		}
+	});
+}
+
+var addToTransaction = function(rows)
+{
+	var id = rows[0].id;
+	var price = rows[0].price;
+	
+	var sql = "INSERT INTO transaction VALUES (" + id + ", 1, " + price + ")";
+	return Promise.resolve(DoQuery(sql));
+}
+
+var updateTransactionTable = function(rows)
+{
+	var id = rows[0].id;
+	
+	var sql = "UPDATE transaction SET quantity = quantity + 1 WHERE id = " + id;
+	return Promise.resolve(DoQuery(sql));
+}
+
+var refreshTransactionTable = function()
+{
+	sql = "SELECT * FROM transaction";
+	return Promise.resolve(DoQuery(sql));
 }
 
 
@@ -59,18 +113,24 @@ var onClick = function()
 
 		var sql = "SELECT * FROM inventory WHERE inventory.id = " + id;
 
-		console.log(sql);
-		var pResult = getInventoryItemInfo(sql);
+		var pResult = DoQuery(sql);
 		var clickResolve = Promise.resolve(pResult);
-		clickResolve.then(function(val) {
-			console.log(JSON.stringify(val));
-			var result = JSON.stringify(val);
-			console.log(result);
-			res.send(result);
+		clickResolve.then(function(val) 
+		{
+			//console.log(val[0].item);
+			//val = JSON.stringify(val);
+			
+			var result = getPriceFromID(val[0].id).then(doesItemExistInTransaction).then(refreshTransactionTable).then(function(rows)
+			{
+				console.log(rows);
+				res.send(rows);
+			});
+			
 		});
 		
 	});
 }
+
 
 /*app.get("/click",function(req,res){
   var id = req.param('id');
